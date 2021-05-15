a260467e383d57ed2730cc989bc25a4375b7162e
Dang Manh Cuong <dangmc95@gmail.com>
2021-05-15 17:14:53 +0700

    reefactor

--- transport/urls.py
+++ transport/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
+from django.urls import path, reverse_lazy
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('vehicle/', views.vehicle, name='vehicle'),
     path('driver/', views.driver, name='driver'),
+    path('manager/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='manager'),
 ]
